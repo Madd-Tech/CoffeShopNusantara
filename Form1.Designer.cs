8260420a20f10738a57cf8c74bb2e8cc8869179e
Deadlyzz021006 <khoirullohmuhammadr@gmail.com>
2026-02-21 07:54:31 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace CoffeShopNusantara
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            label3 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.BackColor = Color.FromArgb(255, 128, 0);
+            splitContainer1.Panel1.BackgroundImage = Properties.Resources.Caffe_Mocha_Magic_For_Chocolate_Coffee_Lovers___Happy_Baking_Days;
+            splitContainer1.Panel1.Controls.Add(label3);
+            splitContainer1.Panel1.Controls.Add(label2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.BackgroundImage = Properties.Resources.download__15_;
+            splitContainer1.Panel2.Controls.Add(label5);
+            splitContainer1.Panel2.Controls.Add(textBox2);
+            splitContainer1.Panel2.Controls.Add(label4);
+            splitContainer1.Panel2.Controls.Add(textBox1);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Size = new Size(1261, 857);
+            splitContainer1.SplitterDistance = 738;
+            splitContainer1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Black;
+            label3.Font = new Font("Verdana", 26F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(255, 128, 0);
+            label3.Location = new Point(266, 128);
+            label3.Name = "label3";
+            label3.Size = new Size(331, 62);
+            label3.TabIndex = 1;
+            label3.Text = "Nusantara";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 26F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(155, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(344, 62);
+            label2.TabIndex = 0;
+            label2.Text = "CoffeShop ";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.MenuText;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.ForeColor = SystemColors.Menu;
+            textBox1.Location = new Point(101, 243);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(332, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DarkGoldenrod;
+            label1.Font = new Font("Showcard Gothic", 20F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(138, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(281, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Login Admin";
+            label1.Click += label1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(101, 208);
+            label4.Name = "label4";
+            label4.Size = new Size(121, 32);
+            label4.TabIndex = 2;
+            label4.Text = "username";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.MenuText;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.ForeColor = SystemColors.Menu;
+            textBox2.Location = new Point(101, 342);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(332, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Yu Gothic UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonFace;
+            label5.Location = new Point(101, 307);
+            label5.Name = "label5";
+            label5.Size = new Size(116, 32);
+            label5.TabIndex = 4;
+            label5.Text = "password";
+            label5.Click += label5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1261, 857);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label5;
+        private TextBox textBox2;
+        private Label label4;
+    }
+}
